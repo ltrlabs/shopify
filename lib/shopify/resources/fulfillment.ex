0eb712f2474bfa1a48d379cba8183d22a0e5083b
Piotr Marat <piotrek.marat@gmail.com>
2017-08-03 15:52:20 +0200

    add fulfillment

--- lib/shopify/resources/fulfillment.ex
+++ lib/shopify/resources/fulfillment.ex
@@ -1,7 +1,19 @@
 defmodule Shopify.Fulfillment do
   @moduledoc false
-  
   @derive [Poison.Encoder]
+  @singular "fulfillment"
+  @plural "fulfillments"
+
+  alias Shopify.{
+    Fulfillment
+  }
+
+  use Shopify.NestedResource, import: [
+    :find,
+    :all,
+    :create,
+    :update
+  ]
 
   defstruct [
     :created_at,
@@ -19,4 +31,30 @@ defmodule Shopify.Fulfillment do
     :service,
     :shipment_status,
   ]
+
+  def cancel(%Session{} = session, top_id, nest_id) do
+    body = "{}"
+    session
+      |> Request.new(cancel_url(top_id, nest_id), %{}, singular_resource(), body)
+      |> Client.post
+  end
+
+  @doc false
+  def empty_resource do
+    %Fulfillment{}
+  end
+
+  @doc false
+  def find_url(top_id, nest_id) do 
+    "orders/#{top_id}/" <> @plural <>  "/#{nest_id}.json"
+  end
+
+  def cancel_url(top_id, nest_id) do 
+    "orders/#{top_id}/" <> @plural <>  "/#{nest_id}/cancel.json"
+  end
+
+  @doc false
+  def all_url(top_id) do
+    "orders/#{top_id}/" <> @plural <> ".json"
+  end
 end
